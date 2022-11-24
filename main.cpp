a1bdb9349acbd1e1deb0d7f0ac8f11be7434be35
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-11-24 20:58:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    int a=1, b=1, c, n;
+    cin>>n;
+
+    while (a<=n) {
+        cout<<a<<endl;
+        c = a;
+        a = b;
+        b = a + c;
+    }
+
+    return 0;
 }
